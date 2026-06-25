7548914918a1f75ead19920ff269e304848e57e6
mkshsmwl10-web <mkshsmwl10@gmail.com>
2026-06-25 13:40:58 +0530

    Update fetch_nifty.py

--- fetch_nifty.py
+++ fetch_nifty.py
@@ -1,1 +1,13 @@
 print("Python Working")
+import json
+
+data = {
+    "spot": 25000,
+    "atm": 25000,
+    "time": "test"
+}
+
+with open("option_data.json", "w") as f:
+    json.dump(data, f)
+
+print("JSON Created")
